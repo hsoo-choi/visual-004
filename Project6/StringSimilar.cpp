035d5fa02c40efe78c630e057f86191c259cf42a
hsoo.choi <choihszg@gmail.com>
2023-07-06 11:26:54 +0900

    [feature] double length

--- Project6/StringSimilar.cpp
+++ Project6/StringSimilar.cpp
@@ -9,11 +9,31 @@ class StringSimilar
 public:
 	int GetScoreWithLength(const vector<string>& strs)
 	{
+		int score = -1;
+		size_t longLength, shortLength;
 		string str1 = strs[0];
 		string str2 = strs[1];
 
-		if (str1.length() == str2.length()) return 60;
+		if (str1.length() >= str2.length())
+		{
+			longLength = str1.length();
+			shortLength = str2.length();
+		}
+		else
+		{
+			longLength = str2.length();
+			shortLength = str1.length();
+		}
+		
+		if (str1.length() == str2.length())
+		{
+			score = 60;
+		}
+		else if(longLength >= shortLength * 2)
+		{
+			score = 0;
+		}
 
-		return 0;
+		return score;
 	}
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -4,9 +4,23 @@
 #include <string>
 using namespace std;
 
-TEST(SimilarTest, CheckLength) {
+TEST(SimilarTest, CheckScoreForSameLength) {
 	StringSimilar ss;
 	vector<string> strs = { "abc", "def" };
 
 	EXPECT_EQ(60, ss.GetScoreWithLength(strs));  
+}
+
+TEST(SimilarTest, CheckScoreForDoubleLength) {
+	StringSimilar ss;
+	vector<string> strs = { "a", "bb" };
+
+	EXPECT_EQ(0, ss.GetScoreWithLength(strs));
+}
+
+TEST(SimilarTest, CheckScoreForDiffrentLength) {
+	StringSimilar ss;
+	vector<string> strs = { "aaabb", "baa" };
+
+	EXPECT_EQ(0, ss.GetScoreWithLength(strs));
 }
