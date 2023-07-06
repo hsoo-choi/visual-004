e43b20f4d4124bb669de89ed2bf90e94de405292
hsoo.choi <choihszg@gmail.com>
2023-07-06 11:17:02 +0900

    [feature] length

--- Project6/StringSimilar.cpp
+++ Project6/StringSimilar.cpp
@@ -1,4 +1,19 @@
+#include <vector>
+#include <xstring>
+
+#include <string>
+using namespace std;
+
 class StringSimilar
 {
-	
+public:
+	int GetScoreWithLength(const vector<string>& strs)
+	{
+		string str1 = strs[0];
+		string str2 = strs[1];
+
+		if (str1.length() == str2.length()) return 60;
+
+		return 0;
+	}
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,7 +1,12 @@
 #include "pch.h"
 #include "../Project6/StringSimilar.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+#include <string>
+using namespace std;
+
+TEST(SimilarTest, CheckLength) {
+	StringSimilar ss;
+	vector<string> strs = { "abc", "def" };
+
+	EXPECT_EQ(60, ss.GetScoreWithLength(strs));  
 }
